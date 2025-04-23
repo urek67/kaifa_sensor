5f2fa7122cf827f079414d24f3652ac19c869fec
urek67 <57796921+urek67@users.noreply.github.com>
2025-04-23 18:40:58 +0200

    Update __init__.py

--- esphome/components/kaifa_sensor/__init__.py
+++ esphome/components/kaifa_sensor/__init__.py
@@ -1,17 +1,15 @@
 # kaifa_sensor | ESPHome external component | 2025-04-23
 
 from esphome.components import sensor
+from esphome.components.uart import UARTComponent
 import esphome.config_validation as cv
-from esphome import pins
-from esphome import automation
-from esphome.const import CONF_ID
 import esphome.codegen as cg
 
 kaifa_sensor_ns = cg.esphome_ns.namespace("kaifa_sensor")
 KaifaSensor = kaifa_sensor_ns.class_("KaifaSensor", sensor.Sensor, cg.Component)
 
 CONFIG_SCHEMA = sensor.sensor_schema(KaifaSensor).extend({
-    cv.Required("uart_id"): cv.use_id(cg.UARTComponent),
+    cv.Required("uart_id"): cv.use_id(UARTComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
